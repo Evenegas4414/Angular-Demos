b5ca34984a5ede66924a0cdd1c61690760c2bd68
Exequiel Venegas <evenegas4414@gmail.com>
2022-08-05 14:27:56 -0400

    CalculadoraApp

---
+++ CalculadoraApp/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calculadora Angular';
+
+  numero1:number = 0;
+  numero2:number = 0;
+}
